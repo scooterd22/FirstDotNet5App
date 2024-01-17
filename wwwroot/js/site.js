2e2906b6d22f608595ba595765062a235d3afcc0
Scott DiBenedetto <dibenedettoscott@gmail.com>
2024-01-17 15:11:40 -0800

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+function hidewords() {
+    var words = document.getElementById("animation");
+    if (words.style.display === "none") {
+        words.style.display = "block";
+    } else {
+        words.style.display = "none";
+    }
+} setInterval(hidewords, 500);
